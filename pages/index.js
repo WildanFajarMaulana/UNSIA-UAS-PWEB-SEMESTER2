739af46a33f74e182eecdf27b5163ad7c880fc42
rafidaaziz23 <dev.rafidaaziz@gmail.com>
2023-08-06 18:23:20 +0700

    add members data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <div>hello</div>;
-}
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="col-span-12">
+      <h2 className="text-2xl text-slate-800 font-semibold mb-6">
+        Anggota Kelompok 6
+      </h2>
+      <h3 className="text-xl text-slate-800 font-semibold mb-6">
+        Kelas IT-204
+      </h3>
+      <ol>
+        <li>
+          Rafida Aziz - <i>220401070412</i>
+        </li>
+        <li>
+          Bayu Firmansyah - <i>220401070405</i>
+        </li>
+        <li>
+          Wildan Fajar Maulana - <i>220401070414</i>
+        </li>
+        <li>
+          Treido Alde - <i>220401070412</i>
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+export default index;
