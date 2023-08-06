c8598351d83df4a9ee2dcf3bb5d62ea5d9690756
WildanFajarMaulana <wildanfajar71@gmail.com>
2023-08-06 09:30:31 +0700

    change folder struktur and get title subtitle for header

--- pages/components/header.js
+++ pages/components/header.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = ({ title, subtitle }) => {
   return (
     <div className="bg-blue-800 h-52 pt-24 pb-8 rounded-b-2xl px-[120px] flex flex-col gap-3">
-      <span className="text-2xl text-white font-bold">Title</span>
-      <label className="text-xl text-white font-medium">Sub Title</label>
+      <span className="text-2xl text-white font-bold">{title}</span>
+      <label className="text-xl text-white font-medium">{subtitle}</label>
     </div>
   );
 };
--- pages/components/layout.js
+++ pages/components/layout.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Navbar from "./navbar";
 import Footer from "./footer";
 import Header from "./header";
+import { useRouter } from "next/router";
+import Navbar from "./navbar";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+  const path = router.pathname;
+  const pathSegments = path.split("/");
+  const segment = pathSegments[1];
+
+  const capitalizedSegment = segment.charAt(0).toUpperCase() + segment.slice(1);
   return (
     <>
       <Navbar />
-      <Header />
+      <Header title={capitalizedSegment} subtitle={`Sub ${capitalizedSegment}`} />
       <div className="min-h-screen py-[60px] px-[120px]">
         <div className="container mx-auto grid grid-cols-12 gap-8">
           {children}
