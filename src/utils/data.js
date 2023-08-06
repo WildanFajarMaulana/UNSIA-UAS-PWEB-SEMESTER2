e2f917d68c47cc371ba5c2069b94dfc3337c5965
WildanFajarMaulana <wildanfajar71@gmail.com>
2023-08-06 09:13:28 +0700

    change struktur folder components and add folder utils

---
+++ src/utils/data.js
@@ -0,0 +1,19 @@
+export const dataQuiz = [
+  {
+    id: 1,
+    title: "Quiz Pemrograman Web",
+    soal: [
+      {
+        id: 1,
+        text: "Apa tag yang benar untuk membuah teks menjadi bold",
+        pilihan_jawaban: [
+          { id: 1, text: "<i></i>" },
+          { id: 2, text: "<b></b>" },
+          { id: 3, text: "<p></p>" },
+          { id: 4, text: "<span></span>" },
+        ],
+        jawaban_benar: 2,
+      },
+    ],
+  },
+];
